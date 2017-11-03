6dbbb9f81083f73306d52ca9efc830e1031c48a3
unknown <2457675936@qq.com>
2017-11-03 10:38:20 +0800

    First commit.

---
+++ app/src/main/java/com/example/asdf/weatherapplication/gson/Now.java
@@ -0,0 +1,20 @@
+package com.example.asdf.weatherapplication.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2017/10/18.
+ */
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+        @SerializedName("txt")
+        public String info;
+    }
+}
---
+++ app/src/main/java/com/example/asdf/weatherapplication/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.asdf.weatherapplication.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by asdf on 2017/10/13.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequests(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
